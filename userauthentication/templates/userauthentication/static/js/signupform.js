a5c3e58481966c605b70bd9e3413fc3b5bc5475b
Jayaprakash_dev <jp659641@gmail.com>
2022-06-29 14:31:25 +0530

    Modified: user signup and register forms

--- userauthentication/templates/userauthentication/static/js/signupform.js
+++ userauthentication/templates/userauthentication/static/js/signupform.js
@@ -1,19 +1,51 @@
-$("input[name='password2']").keyup(checkPassword);
+$("input[type=password]").attr('placeholder', 'must be 8 characters');
+$("input[name='password2']").keyup(checkForm);
+$("input[name='first_name']").keyup(checkForm);
+$("input[name='last_name']").keyup(checkForm);
 
-function checkPassword(e) {
+function checkForm(e) {
     e.preventDefault();
 
-    var _password_1 = $("input[name='password1']").val()
-    var _password_2 = $("input[name='password2']").val()
+    var _username_input = $('input[name="username"]').val();
+    var _email_input = $('input[type=email]').val();
+    var _first_name = $("input[name='first_name']").val();
+    var _last_name = $("input[name='last_name']").val();
+    var _password_1 = $("input[name='password1']").val();
+    var _password_2 = $("input[name='password2']").val();
     var _csrf_token = $("input[type=hidden]").val();
 
-    if(_password_2.length >= 8) {
+    if(_password_2.length >= 3) {
         $.ajax({
             type: 'POST',
             url: '',
-            data: {'csrfmiddlewaretoken': _csrf_token, 
-                    'password1': _password_1,
-                    'password2': _password_2
+            data: {
+                    csrfmiddlewaretoken: _csrf_token, 
+                    username: _username_input,
+                    email: _email_input,
+                    first_name: _first_name,
+                    last_name: _last_name, 
+                    password1: _password_1,
+                    password2: _password_2,
+            },
+            success: (data) => {
+                if (data.message === undefined) {
+                    $('.message').empty();
+                }
+                else {
+                    $('.message').html(data.message);
+                }
+            }
+        });
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '',
+            data: {
+                    csrfmiddlewaretoken: _csrf_token,
+                    first_name: _first_name,
+                    last_name: _last_name, 
+                    password1: 0,
+                    password2: 1,
             },
             success: (data) => {
                 if (data.message === undefined) {
--- userauthentication/views.py
+++ userauthentication/views.py
@@ -1,4 +1,5 @@
-import email
+import re
+import redis
 from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -6,6 +7,12 @@ from django.views import View
 from django.contrib.auth.models import User
 
 from .forms import UserRegistraionForm, UserSignupForm, UserLoginForm
+
+# redis
+redis_client = redis.Redis(
+    host='127.0.0.1', port=6379
+)
+
 
 class UserLoginView(View):
 
@@ -19,18 +26,30 @@ class UserRegisterView(View):
     def get(self, req):
         _registration_form = UserRegistraionForm()
         return render(req, 'userauthentication/userregistration.html', {'form': _registration_form})
-    
+
     def post(self, req):
         _username = req.POST.get('username')
         _email = req.POST.get('email')
 
+        # checks username already exists in the database or not
         if User.objects.filter(username=_username).exists():
             data = {'message': "username already taken"}
             return JsonResponse(data)
-        
+
+        # checks the user entered is valid or not with regex pattern
+        if len(_username) > 2:
+            if re.search("^[\w.@+-]+\Z", _username) is None:
+                data = {'message': "username is not valid"}
+                return JsonResponse(data)
+
+        # checks the email already exists in the database or not
         if User.objects.filter(email=_email).exists():
             data = {'message': "email is already registered"}
             return JsonResponse(data)
+
+        # if username & email is valid, it will be updated in the redis memeory
+        redis_client.set('username', _username)
+        redis_client.set('email', _email)
 
         return HttpResponseRedirect(reverse('userauthentication:signup'))
 
@@ -38,24 +57,42 @@ class UserRegisterView(View):
 class UserSignUpView(View):
 
     def get(self, req):
-        _signup_form = UserSignupForm()
+        initial_data = {
+            'username': (redis_client.get('username').decode('utf-8')),
+            'email': str(redis_client.get('email').decode('utf-8'))
+        }
+        _signup_form = UserSignupForm(initial=initial_data)
         return render(req, 'userauthentication/usersignup.html', {'form': _signup_form})
 
     def post(self, req):
+
+        _first_name = req.POST.get('first_name')
+        _last_name = req.POST.get('last_name')
         _password_1 = req.POST.get('password1')
         _password_2 = req.POST.get('password2')
 
-        if _password_1 == _password_2:
-            _registration_form = UserSignupForm(req.POST)
-            data = {'message': "password correct"}
-            JsonResponse(data)
+        if len(_first_name) > 0 or len(_last_name) > 0:
+            if re.search("^[\w.@+-]+\Z", _first_name) is None:
+                data = {'message': "first or last name is not valid"}
+                return JsonResponse(data)
 
-            if _registration_form.is_valid():
-                _registration_form.save()
+            if re.search("^[\w.@+-]+\Z", _last_name) is None:
+                data = {'message': "first or last name is not valid"}
+                return JsonResponse(data)
+
+        if len(_password_1) >= 8:
+            if _password_1 == _password_2:
+                
+                _signup_form = UserSignupForm(req.POST)
+                
+                if _signup_form.is_valid():
+                    _signup_form.save()
+
             else:
-                print(_registration_form.errors.as_data())
+                data = {'message': "password mismatching"}
+                return JsonResponse(data)
+
         else:
-            data = {'message': "password mismatching"}
-            return JsonResponse(data)
-        
+            return JsonResponse({'message': ''})
+
         return HttpResponseRedirect(reverse('userauthentication:register'))
